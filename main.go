9cc0af46f9933fde743ae17531d1b9ee9745f01b
hrishabpachange <hrishabpachange02@gmail.com>
2025-12-16 17:05:46 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type params struct { //Allocates a memory space in the local memory
+
+}
+
+func main() {
+
+	//custom ServeMux
+	router := http.NewServeMux()
+
+	//handles root functionality GET /
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request)  {
+		w.Write([]byte("Hello"))
+	})
+
+	//handles POST functionality to shorten URL
+	router.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("This is POST /post endpoint"))
+	})
+
+	//create custom server
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	fmt.Println("Listening on port: 8080")
+	server.ListenAndServe()
+}
